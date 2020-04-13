0121cefb2464c18dabd55de2d8d71d12b299f5e2
Andrew Yu <andrewthomasyu@gmail.com>
2020-04-13 23:47:13 +0800

    Fix to account for change in data model

--- db_load.py
+++ db_load.py
@@ -5,13 +5,14 @@ import pandas as pd
 df = pd.read_csv('data/players_20.csv')
 df = df[:100]
 for row in df.itertuples():
+    id = getattr(row,'Index')
     sofifa_id=getattr(row,'sofifa_id')
     club=getattr(row,'club')
     country=getattr(row,'nationality')
     short_name=getattr(row,'short_name')
     long_name=getattr(row,'long_name')
     
-    player = Player(sofifa_id=sofifa_id,club=club,country=country,
+    player = Player(player_id=id,sofifa_id=sofifa_id,club=club,country=country,
     short_name=short_name,long_name=long_name)
     db.session.add(player)
     db.session.commit()
