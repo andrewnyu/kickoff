0f0a232b02c9e7fc5c074e08b31f3e3562e313be
Andrew Yu <andrewthomasyu@gmail.com>
2020-05-06 21:45:49 +0800

    Remove numpy and pandas dependency from init_db

--- init_db.py
+++ init_db.py
@@ -1,9 +1,9 @@
 from flask import Flask
-import pandas as pd 
 import os
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from app.models import Player
+import csv
 
 #Issue for this function is how to integrate into create_app
 
@@ -14,15 +14,17 @@ db = SQLAlchemy(temp)
 
 
 #load data into db
-df = pd.read_csv('app/static/data/players_20.csv')
-df = df[:100]
-for row in df.itertuples():
-    id = getattr(row,'Index')
-    sofifa_id=getattr(row,'sofifa_id')
-    club=getattr(row,'club')
-    country=getattr(row,'nationality')
-    short_name=getattr(row,'short_name')
-    long_name=getattr(row,'long_name')
+df = csv.DictReader('app/static/data/players_20.csv', delimiter=',')
+
+index = 0
+for row in df:
+    id = index
+    sofifa_id=['sofifa_id']
+    club=['club']
+    country=['nationality']
+    short_name=['short_name']
+    long_name=['long_name']
+    index +=1
     
     player = Player(player_id=id,sofifa_id=sofifa_id,club=club,country=country,
     short_name=short_name,long_name=long_name)
